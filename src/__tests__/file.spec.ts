05f3c1e731c74847dff6802ba58d0b18d77868e2
Jakub Rożek <jakub@stoplight.io>
2020-07-09 19:18:42 +0200

    fix: urijs type

--- src/__tests__/file.spec.ts
+++ src/__tests__/file.spec.ts
@@ -1,16 +1,17 @@
 import { readFile } from 'fs';
 import { resolveFile } from '../file';
+import * as URI from 'urijs';
 
 jest.mock('fs');
 
 describe('resolveFile()', () => {
   it('works', async () => {
     ((readFile as unknown) as jest.Mock).mockImplementation((_, __, cb) => cb(undefined, 'root:*'));
-    await expect(resolveFile({ path: () => '/etc/passwd' } as uri.URI)).resolves.toEqual('root:*');
+    await expect(resolveFile({ path: () => '/etc/passwd' } as URI)).resolves.toEqual('root:*');
   });
 
   it('handles failures', async () => {
     ((readFile as unknown) as jest.Mock).mockImplementation((_, __, cb) => cb(new Error('Using shadow, ha, ha')));
-    await expect(resolveFile({ path: () => '/etc/passwd' } as uri.URI)).rejects.toThrowError('Using shadow, ha, ha');
+    await expect(resolveFile({ path: () => '/etc/passwd' } as URI)).rejects.toThrowError('Using shadow, ha, ha');
   });
 });
